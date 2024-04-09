852f64f04ab27a06cb0009979f68934fd612cb6f
micgaz <michal.gazinski@gmail.com>
2024-04-09 08:59:45 +0200

    Task 6 exercise1 task 3:Test the access control:part2

--- Task06_6.sh
+++ Task06_6.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-cat /tmp/hero_files/message.txt # READ operation
-echo "New message" > /tmp/hero_files/message.txt # WRITE operation
-su - hero
-whoami
+sudo usermod -aG heroes kali
 id
