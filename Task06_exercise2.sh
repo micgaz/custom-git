56821dcbd6ce6255b865ad6789ae6d502c9dd4f2
micgaz <michal.gazinski@gmail.com>
2024-04-09 09:09:22 +0200

    Task 6 exercise2 p2: Reading user input task1

--- Task06_exercise2.sh
+++ Task06_exercise2.sh
@@ -5,3 +5,7 @@ if [[ "${UID}" -ne 0 ]]; then
     echo 'Please run the script using sudo or as a root.'
     exit 1 # Exit with an error
 fi
+# Prompt for user input
+read -p 'Enter the username to create: ' USER_NAME
+read -p 'Enter the name of the person or application that will be using this account: ' COMMENT
+read -p 'Enter the password to use for the account: ' PASSWORD
